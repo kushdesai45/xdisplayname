190fc6cbe0836be78cad4fd405c7cd9e8e9b5df4
kush desai <kush@artiumacademy.com>
2024-09-11 11:59:05 +0530

    display name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [fullName,setFullName] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFullName(firstName+" "+lastName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Full Name Display</h1>
+      <form action="submit" onSubmit={onSubmit}>
+      <div>
+      <span>First Name:</span> 
+        <input type="text" name="First Name" required onChange={(e)=>setFirstName(e.target.value)}/>
+        </div>
+        <div>
+        <span>Last Name:</span> 
+        <input type="text" name="Last Name" id="" required onChange={(e)=>setLastName(e.target.value)}/>
+        </div>
+        <button type="submit" >
+          Submit
+        </button>
+        <p>Full Name: {fullName}</p>
+      </form>
     </div>
   );
 }
